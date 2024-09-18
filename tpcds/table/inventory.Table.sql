f1fd00feee133eec33ebd9fdbd93dfe74c7dec45
mariana-pineda <139593142+mariana-pineda@users.noreply.github.com>
2024-09-18 18:23:30 -0500

    Delete tpcds/table directory

--- tpcds/table/inventory.Table.sql
+++
@@ -1,9 +0,0 @@
-
-CREATE TABLE tpcds.inventory (
-	inv_date_sk INTEGER NOT NULL, 
-	inv_item_sk INTEGER NOT NULL, 
-	inv_warehouse_sk INTEGER NOT NULL, 
-	inv_quantity_on_hand INTEGER, 
-	CONSTRAINT inventory_pkey PRIMARY KEY (inv_date_sk, inv_item_sk, inv_warehouse_sk)
-) DISTSTYLE KEY DISTKEY (inv_item_sk) SORTKEY (inv_date_sk)
-
--- tpcds/table/reason.Table.sql
+++
@@ -1,8 +0,0 @@
-
-CREATE TABLE tpcds.reason (
-	r_reason_sk INTEGER NOT NULL, 
-	r_reason_id CHAR(16) NOT NULL, 
-	r_reason_desc CHAR(100), 
-	CONSTRAINT reason_pkey PRIMARY KEY (r_reason_sk)
-) DISTSTYLE ALL
-
