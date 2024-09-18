f1cef73bd679af113e538cdc8f14cda3320b3950
mariana-pineda <139593142+mariana-pineda@users.noreply.github.com>
2024-09-18 18:23:21 -0500

    Delete tpcds-mig/table directory

--- tpcds-mig/table/inventory.Table.sql
+++
@@ -1,8 +0,0 @@
-
-CREATE TABLE tpcds.inventory (
-	inv_date_sk INTEGER NOT NULL, 
-	inv_item_sk INTEGER NOT NULL, 
-	inv_warehouse_sk INTEGER NOT NULL, 
-	inv_quantity_on_hand INTEGER, 
-	PRIMARY KEY (inv_date_sk, inv_item_sk, inv_warehouse_sk)
-) USING DELTA
