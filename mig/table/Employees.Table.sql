39867a23e9fa864b15acafd2f0b0afccb4c5a5ec
Purgo AI <support@purgo.ai>
2024-09-23 11:26:25 -0500

    Commit message

---
+++ mig/table/Employees.Table.sql
@@ -0,0 +1,25 @@
+
+CREATE TABLE Employees (
+  EmployeeID INT NOT NULL,
+  LastName STRING NOT NULL,
+  FirstName STRING NOT NULL,
+  Title STRING,
+  TitleOfCourtesy STRING,
+  BirthDate TIMESTAMP,
+  HireDate TIMESTAMP,
+  Address STRING,
+  City STRING,
+  Region STRING,
+  PostalCode STRING,
+  Country STRING,
+  HomePhone STRING,
+  Extension STRING,
+  Photo BINARY,
+  Notes STRING,
+  ReportsTo INT,
+  PhotoPath STRING
+);
+
+ALTER TABLE Employees ADD CONSTRAINT PK_Employees PRIMARY KEY (EmployeeID);
+
+ALTER TABLE Employees ADD CONSTRAINT FK_Employees_Employees FOREIGN KEY (ReportsTo) REFERENCES Employees (EmployeeID);
