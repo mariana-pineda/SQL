7aebb5d4eea639b4b3bdf29c06f600041cdcb6da
Purgo AI <support@purgo.ai>
2024-09-30 13:13:34 -0500

    Commit message

---
+++ dbo10-mig/table/Products.Table.sql
@@ -0,0 +1,16 @@
+
+CREATE TABLE Products (
+    ProductID INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY, 
+    ProductName STRING NOT NULL, 
+    SupplierID INTEGER, 
+    CateryID INTEGER, 
+    QuantityPerUnit STRING, 
+    UnitPrice DECIMAL(19,4) DEFAULT 0, 
+    UnitsInStock SMALLINT DEFAULT 0, 
+    UnitsOnOrder SMALLINT DEFAULT 0, 
+    ReorderLevel SMALLINT DEFAULT 0, 
+    Discontinued BOOLEAN NOT NULL DEFAULT false, 
+    PRIMARY KEY (ProductID), 
+    FOREIGN KEY (CateryID) REFERENCES Cateries(CateryID), 
+    FOREIGN KEY (SupplierID) REFERENCES Suppliers(SupplierID)
+)
