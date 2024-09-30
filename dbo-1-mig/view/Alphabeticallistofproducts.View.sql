545e085937703c616b625cf695520fccddbaed73
Purgo AI <support@purgo.ai>
2024-09-30 14:59:23 -0500

    Commit message

---
+++ dbo-1-mig/view/Alphabeticallistofproducts.View.sql
@@ -0,0 +1,5 @@
+
+CREATE OR REPLACE VIEW `Alphabetical list of products` AS
+SELECT Products.*, Categories.CategoryName
+FROM Categories INNER JOIN Products ON Categories.CategoryID = Products.CategoryID
+WHERE Products.Discontinued = 0
