eb7e6db57a2cbb55090a495d28f2262d16527e01
Purgo AI <support@purgo.ai>
2024-10-01 15:19:22 -0500

    Commit message

--- dbo-mig/view/CurrentProductList.View.sql
+++ dbo-mig/view/CurrentProductList.View.sql
@@ -2,5 +2,5 @@
 CREATE OR REPLACE VIEW `Current Product List` AS
 SELECT Product_List.ProductID, Product_List.ProductName
 FROM Products AS Product_List
-WHERE Product_List.Discontinued = 0;
--- ORDER BY Product_List.ProductName
+WHERE Product_List.Discontinued = 0
+
