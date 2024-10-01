68fe06e520bbce15206b6dfd3a27124a6d3d39c5
Purgo AI <support@purgo.ai>
2024-10-01 14:53:48 -0500

    Commit message

--- dbo-mig/view/SalesTotalsbyAmount.View.sql
+++ dbo-mig/view/SalesTotalsbyAmount.View.sql
@@ -4,5 +4,4 @@ SELECT `Order Subtotals`.Subtotal AS SaleAmount, Orders.OrderID, Customers.Compa
 FROM Customers 
 INNER JOIN Orders ON Customers.CustomerID = Orders.CustomerID
 INNER JOIN `Order Subtotals` ON Orders.OrderID = `Order Subtotals`.OrderID
-WHERE `Order Subtotals`.Subtotal > 2500 
-AND Orders.ShippedDate BETWEEN '1997-01-01' AND '1997-12-31'
+WHERE `Order Subtotals`.Subtotal > 2500 AND Orders.ShippedDate BETWEEN '1997-01-01' AND '1997-12-31'
