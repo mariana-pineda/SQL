116247212e281bf6868a1a07b69201241b898360
Purgo AI <support@purgo.ai>
2024-10-01 15:18:52 -0500

    Commit message

--- dbo-mig/table/EmployeeTerritories.Table.sql
+++ dbo-mig/table/EmployeeTerritories.Table.sql
@@ -1,8 +1,8 @@
 
 CREATE TABLE EmployeeTerritories (
-  EmployeeID INTEGER NOT NULL,
-  TerritoryID STRING NOT NULL,
+  EmployeeID INT NOT NULL, 
+  TerritoryID STRING NOT NULL, 
   PRIMARY KEY (EmployeeID, TerritoryID),
   FOREIGN KEY (EmployeeID) REFERENCES Employees (EmployeeID),
   FOREIGN KEY (TerritoryID) REFERENCES Territories (TerritoryID)
-) USING DELTA;
+)
