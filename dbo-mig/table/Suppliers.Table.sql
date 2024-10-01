9ad8a5c456e7a510a787b463b5873988d4fed506
Purgo AI <support@purgo.ai>
2024-10-01 14:53:14 -0500

    Commit message

--- dbo-mig/table/Suppliers.Table.sql
+++ dbo-mig/table/Suppliers.Table.sql
@@ -1,18 +1,16 @@
 
 CREATE TABLE Suppliers (
-    SupplierID INT NOT NULL,
-    CompanyName STRING NOT NULL,
-    ContactName STRING,
-    ContactTitle STRING,
-    Address STRING,
-    City STRING,
-    Region STRING,
-    PostalCode STRING,
-    Country STRING,
-    Phone STRING,
-    Fax STRING,
-    HomePage STRING
+  SupplierID INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY, 
+  CompanyName STRING NOT NULL, 
+  ContactName STRING, 
+  ContactTitle STRING, 
+  Address STRING, 
+  City STRING, 
+  Region STRING, 
+  PostalCode STRING, 
+  Country STRING, 
+  Phone STRING, 
+  Fax STRING, 
+  HomePage STRING, 
+  PRIMARY KEY (SupplierID)
 )
-USING DELTA;
-
-ALTER TABLE Suppliers ADD CONSTRAINT PK_Suppliers PRIMARY KEY (SupplierID);
