f44f3b14f24caee969e63636bbda825c3c71f2f7
Purgo AI <support@purgo.ai>
2024-10-01 15:18:40 -0500

    Commit message

--- dbo-mig/table/Cateries.Table.sql
+++ dbo-mig/table/Cateries.Table.sql
@@ -1,8 +1,15 @@
 
 CREATE TABLE Cateries (
-  CateryID INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY,
-  CateryName STRING NOT NULL,
-  Description STRING,
-  Picture BINARY,
+  CateryID INT NOT NULL, 
+  CateryName STRING NOT NULL, 
+  Description STRING, 
+  Picture BINARY, 
   PRIMARY KEY (CateryID)
-)
+);
+
+CREATE SEQUENCE CateryID_seq START WITH 1 INCREMENT BY 1;
+
+CREATE TRIGGER before_insert_Cateries
+BEFORE INSERT ON Cateries
+FOR EACH ROW
+SET NEW.CateryID = nextval('CateryID_seq');
