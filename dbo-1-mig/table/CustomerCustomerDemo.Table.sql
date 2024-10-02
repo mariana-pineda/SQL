e609750c304056a8602743b7a00020cda4b5b227
Purgo AI <support@purgo.ai>
2024-10-02 17:11:25 -0500

    Commit message

--- dbo-1-mig/table/CustomerCustomerDemo.Table.sql
+++ dbo-1-mig/table/CustomerCustomerDemo.Table.sql
@@ -4,16 +4,11 @@ CREATE TABLE CustomerCustomerDemo (
   CustomerTypeID STRING NOT NULL
 );
 
--- Adding Primary Key Constraint
-ALTER TABLE CustomerCustomerDemo
-ADD CONSTRAINT PK_CustomerCustomerDemo
-PRIMARY KEY (CustomerID, CustomerTypeID);
+ALTER TABLE CustomerCustomerDemo 
+ADD CONSTRAINT PK_CustomerCustomerDemo PRIMARY KEY (CustomerID, CustomerTypeID);
 
--- Adding Foreign Key Constraints
-ALTER TABLE CustomerCustomerDemo
-ADD CONSTRAINT FK_CustomerCustomerDemo
-FOREIGN KEY (CustomerTypeID) REFERENCES CustomerDemographics (CustomerTypeID);
+ALTER TABLE CustomerCustomerDemo 
+ADD CONSTRAINT FK_CustomerCustomerDemo FOREIGN KEY (CustomerTypeID) REFERENCES CustomerDemographics (CustomerTypeID);
 
-ALTER TABLE CustomerCustomerDemo
-ADD CONSTRAINT FK_CustomerCustomerDemo_Customers
-FOREIGN KEY (CustomerID) REFERENCES Customers (CustomerID);
+ALTER TABLE CustomerCustomerDemo 
+ADD CONSTRAINT FK_CustomerCustomerDemo_Customers FOREIGN KEY (CustomerID) REFERENCES Customers (CustomerID);
