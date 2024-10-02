e0d42db54aaffc4e08ca8dca5b8fd53d8da8e53a
Purgo AI <support@purgo.ai>
2024-10-02 15:39:56 -0500

    Commit message

--- dbo10-mig/table/OrderDetails.Table.sql
+++ dbo10-mig/table/OrderDetails.Table.sql
@@ -1,14 +1,18 @@
 
 CREATE TABLE `Order Details` (
-  OrderID INT NOT NULL, 
-  ProductID INT NOT NULL, 
-  UnitPrice DECIMAL(19,4) NOT NULL DEFAULT 0, 
-  Quantity SMALLINT NOT NULL DEFAULT 1, 
-  Discount FLOAT NOT NULL DEFAULT 0
-);
+  `OrderID` INT NOT NULL,
+  `ProductID` INT NOT NULL,
+  `UnitPrice` DECIMAL(19,4) NOT NULL DEFAULT 0,
+  `Quantity` SMALLINT NOT NULL DEFAULT 1,
+  `Discount` FLOAT NOT NULL DEFAULT 0
+)
+USING DELTA;
 
-ALTER TABLE `Order Details` ADD CONSTRAINT PK_Order_Details PRIMARY KEY (OrderID, ProductID);
+ALTER TABLE `Order Details`
+ADD CONSTRAINT PK_Order_Details PRIMARY KEY (OrderID, ProductID);
 
-ALTER TABLE `Order Details` ADD CONSTRAINT FK_Order_Details_Orders FOREIGN KEY (OrderID) REFERENCES Orders (OrderID);
+ALTER TABLE `Order Details`
+ADD CONSTRAINT FK_Order_Details_Orders FOREIGN KEY (OrderID) REFERENCES Orders (OrderID);
 
-ALTER TABLE `Order Details` ADD CONSTRAINT FK_Order_Details_Products FOREIGN KEY (ProductID) REFERENCES Products (ProductID);
+ALTER TABLE `Order Details`
+ADD CONSTRAINT FK_Order_Details_Products FOREIGN KEY (ProductID) REFERENCES Products (ProductID);
