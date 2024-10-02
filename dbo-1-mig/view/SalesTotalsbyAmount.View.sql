fb87f79486df912b80d1dbcfcda0529b4c732d94
Purgo AI <support@purgo.ai>
2024-10-02 17:12:05 -0500

    Commit message

--- dbo-1-mig/view/SalesTotalsbyAmount.View.sql
+++ dbo-1-mig/view/SalesTotalsbyAmount.View.sql
@@ -1,5 +1,5 @@
 
-CREATE OR REPLACE VIEW `Sales Totals by Amount` AS
+CREATE VIEW `Sales Totals by Amount` AS
 SELECT `Order Subtotals`.Subtotal AS SaleAmount, Orders.OrderID, Customers.CompanyName, Orders.ShippedDate
 FROM Customers 
 INNER JOIN Orders ON Customers.CustomerID = Orders.CustomerID
