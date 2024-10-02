fff8c02f7aa6dbcce2c7c487b670c58741e64a8e
Purgo AI <support@purgo.ai>
2024-10-02 15:40:22 -0500

    Commit message

--- dbo10-mig/table/Products.Table.sql
+++ dbo10-mig/table/Products.Table.sql
@@ -1,16 +1,19 @@
 
 CREATE TABLE Products (
-    ProductID INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY, 
-    ProductName STRING NOT NULL, 
-    SupplierID INTEGER, 
-    CateryID INTEGER, 
-    QuantityPerUnit STRING, 
-    UnitPrice DECIMAL(19,4) DEFAULT 0, 
-    UnitsInStock SMALLINT DEFAULT 0, 
-    UnitsOnOrder SMALLINT DEFAULT 0, 
-    ReorderLevel SMALLINT DEFAULT 0, 
-    Discontinued BOOLEAN NOT NULL DEFAULT false, 
-    PRIMARY KEY (ProductID), 
-    FOREIGN KEY (CateryID) REFERENCES Cateries(CateryID), 
-    FOREIGN KEY (SupplierID) REFERENCES Suppliers(SupplierID)
-)
+    ProductID INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY,
+    ProductName STRING NOT NULL,
+    SupplierID INTEGER,
+    CaterID INTEGER,
+    QuantityPerUnit STRING,
+    UnitPrice DECIMAL(19, 4) DEFAULT 0,
+    UnitsInStock SMALLINT DEFAULT 0,
+    UnitsOnOrder SMALLINT DEFAULT 0,
+    ReorderLevel SMALLINT DEFAULT 0,
+    Discontinued BOOLEAN NOT NULL DEFAULT false
+);
+
+ALTER TABLE Products ADD CONSTRAINT PK_Products PRIMARY KEY (ProductID);
+
+ALTER TABLE Products ADD CONSTRAINT FK_Products_Cateries FOREIGN KEY (CaterID) REFERENCES Cateries (CaterID);
+
+ALTER TABLE Products ADD CONSTRAINT FK_Products_Suppliers FOREIGN KEY (SupplierID) REFERENCES Suppliers (SupplierID);
