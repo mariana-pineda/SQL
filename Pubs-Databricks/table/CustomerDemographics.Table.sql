e1d4eb53a771892ef8afc200ef45501a27d624c1
Purgo AI <support@purgo.ai>
2024-10-02 12:05:40 -0500

    Commit message

---
+++ Pubs-Databricks/table/CustomerDemographics.Table.sql
@@ -0,0 +1,9 @@
+
+CREATE TABLE CustomerDemographics (
+  CustomerTypeID STRING NOT NULL, 
+  CustomerDesc STRING NULL
+) 
+USING DELTA;
+
+ALTER TABLE CustomerDemographics
+ADD CONSTRAINT PK_CustomerDemographics PRIMARY KEY (CustomerTypeID);
