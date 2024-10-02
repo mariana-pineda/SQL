44d830318d3586993d4c2de1ec1fb26bdcde7962
Purgo AI <support@purgo.ai>
2024-10-02 15:39:43 -0500

    Commit message

--- dbo10-mig/table/EmployeeTerritories.Table.sql
+++ dbo10-mig/table/EmployeeTerritories.Table.sql
@@ -1,8 +1,8 @@
 
 CREATE TABLE EmployeeTerritories (
-  EmployeeID INT NOT NULL, 
-  TerritoryID STRING NOT NULL, 
-  CONSTRAINT PK_EmployeeTerritories PRIMARY KEY (EmployeeID, TerritoryID), 
-  FOREIGN KEY (EmployeeID) REFERENCES Employees (EmployeeID), 
-  FOREIGN KEY (TerritoryID) REFERENCES Territories (TerritoryID)
+  EmployeeID INTEGER NOT NULL,
+  TerritoryID STRING NOT NULL,
+  PRIMARY KEY (EmployeeID, TerritoryID),
+  FOREIGN KEY (EmployeeID) REFERENCES Employees(EmployeeID),
+  FOREIGN KEY (TerritoryID) REFERENCES Territories(TerritoryID)
 )
