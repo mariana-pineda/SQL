cacea02432a74c5f55c954526bf4c840dd001cf2
Purgo AI <support@purgo.ai>
2024-10-02 15:39:07 -0500

    Commit message

--- dbo10-mig/table/CustomerDemographics.Table.sql
+++ dbo10-mig/table/CustomerDemographics.Table.sql
@@ -1,12 +1,6 @@
 
 CREATE TABLE CustomerDemographics (
-  CustomerTypeID STRING NOT NULL, 
-  CustomerDesc STRING NULL
-)
-USING DELTA
-TBLPROPERTIES (
-  'delta.columnMapping.mode' = 'name'
-);
-
-ALTER TABLE CustomerDemographics
-ADD CONSTRAINT PK_CustomerDemographics PRIMARY KEY (CustomerTypeID);
+  CustomerTypeID STRING NOT NULL,
+  CustomerDesc STRING,
+  PRIMARY KEY (CustomerTypeID)
+) USING DELTA;
