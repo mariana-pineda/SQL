772f31b0d0702019444defb9560844dfa96e3b59
Purgo AI <support@purgo.ai>
2024-10-04 15:37:41 -0500

    Commit message

--- dbo-mig/table/Territories.Table.sql
+++ dbo-mig/table/Territories.Table.sql
@@ -2,7 +2,12 @@
 CREATE TABLE Territories (
   TerritoryID STRING NOT NULL,
   TerritoryDescription STRING NOT NULL,
-  RegionID INT NOT NULL,
-  PRIMARY KEY (TerritoryID),
-  FOREIGN KEY (RegionID) REFERENCES Region (RegionID)
-)
+  RegionID INT NOT NULL
+);
+
+ALTER TABLE Territories
+ADD CONSTRAINT PK_Territories PRIMARY KEY (TerritoryID);
+
+ALTER TABLE Territories
+ADD CONSTRAINT FK_Territories_Region FOREIGN KEY (RegionID)
+REFERENCES Region (RegionID);
