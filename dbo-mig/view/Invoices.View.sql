1db32cc01f20aad25e9ece038f077dc4adb557d2
Purgo AI <support@purgo.ai>
2024-10-04 15:37:52 -0500

    Commit message

--- dbo-mig/view/Invoices.View.sql
+++ dbo-mig/view/Invoices.View.sql
@@ -1,28 +1,23 @@
 
-CREATE VIEW Invoices AS
+CREATE OR REPLACE VIEW Invoices AS
 SELECT Orders.ShipName, Orders.ShipAddress, Orders.ShipCity, Orders.ShipRegion, Orders.ShipPostalCode, 
-    Orders.ShipCountry, Orders.CustomerID, Customers.CompanyName AS CustomerName, Customers.Address, Customers.City, 
-    Customers.Region, Customers.PostalCode, Customers.Country, 
-    (FirstName || ' ' || LastName) AS Salesperson, 
-    Orders.OrderID, Orders.OrderDate, Orders.RequiredDate, Orders.ShippedDate, Shippers.CompanyName AS ShipperName, 
-    `Order Details`.ProductID, Products.ProductName, `Order Details`.UnitPrice, `Order Details`.Quantity, 
-    `Order Details`.Discount, 
-    (CAST(`Order Details`.UnitPrice * Quantity * (1 - Discount) AS DECIMAL(38,2))) AS ExtendedPrice, Orders.Freight
-FROM Shippers 
-INNER JOIN (
-    Products 
-    INNER JOIN (
-        Employees 
-        INNER JOIN (
-            Customers 
-            INNER JOIN Orders 
-            ON Customers.CustomerID = Orders.CustomerID
-        ) 
-        ON Employees.EmployeeID = Orders.EmployeeID
-    ) 
-    INNER JOIN `Order Details` 
-    ON Orders.OrderID = `Order Details`.OrderID
-) 
-ON Products.ProductID = `Order Details`.ProductID 
-ON Shippers.ShipperID = Orders.ShipVia
+	Orders.ShipCountry, Orders.CustomerID, Customers.CompanyName AS CustomerName, Customers.Address, Customers.City, 
+	Customers.Region, Customers.PostalCode, Customers.Country, 
+	(FirstName || ' ' || LastName) AS Salesperson, 
+	Orders.OrderID, Orders.OrderDate, Orders.RequiredDate, Orders.ShippedDate, Shippers.CompanyName AS ShipperName, 
+	OrderDetails.ProductID, Products.ProductName, OrderDetails.UnitPrice, OrderDetails.Quantity, 
+	OrderDetails.Discount, 
+	(CAST(OrderDetails.UnitPrice * OrderDetails.Quantity * (1 - OrderDetails.Discount) AS DECIMAL(19, 4))) AS ExtendedPrice, 
+	Orders.Freight
+FROM 	Shippers 
+INNER JOIN 
+	Orders ON Shippers.ShipperID = Orders.ShipVia
+INNER JOIN 
+	Customers ON Customers.CustomerID = Orders.CustomerID
+INNER JOIN 
+	Employees ON Employees.EmployeeID = Orders.EmployeeID
+INNER JOIN 
+	OrderDetails ON Orders.OrderID = OrderDetails.OrderID
+INNER JOIN 
+	Products ON Products.ProductID = OrderDetails.ProductID
 
