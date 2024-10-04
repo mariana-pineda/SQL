c2199a0c78c7ea885b0122bf8d9b740d6cdf7b1c
Purgo AI <support@purgo.ai>
2024-10-04 15:38:02 -0500

    Commit message

--- dbo-mig/view/ProductsbyCatery.View.sql
+++ dbo-mig/view/ProductsbyCatery.View.sql
@@ -1,14 +1,5 @@
 
-CREATE OR REPLACE VIEW `Products by Catery` AS
-SELECT
-  Cateries.CateryName,
-  Products.ProductName,
-  Products.QuantityPerUnit,
-  Products.UnitsInStock,
-  Products.Discontinued
-FROM
-  Cateries
-INNER JOIN
-  Products ON Cateries.CateryID = Products.CateryID
-WHERE
-  Products.Discontinued <> 1
+CREATE VIEW `Products by Catery` AS
+SELECT Cateries.CateryName, Products.ProductName, Products.QuantityPerUnit, Products.UnitsInStock, Products.Discontinued
+FROM Cateries INNER JOIN Products ON Cateries.CateryID = Products.CateryID
+WHERE Products.Discontinued != 1
