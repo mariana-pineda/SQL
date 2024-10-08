3375c543e09d65e232fbe2977a373b370b94567a
Purgo AI <support@purgo.ai>
2024-10-07 20:08:27 -0500

    Commit message

---
+++ test/view/ProductsAboveAveragePrice.View.sql
@@ -0,0 +1,5 @@
+
+CREATE OR REPLACE VIEW `Products Above Average Price` AS
+SELECT Products.ProductName, Products.UnitPrice
+FROM Products
+WHERE Products.UnitPrice > (SELECT AVG(UnitPrice) FROM Products)
