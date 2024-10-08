6ffb91d0b19e5fc61b5702c8a25d19cb7d2042e0
Purgo AI <support@purgo.ai>
2024-10-07 20:07:29 -0500

    Commit message

---
+++ test/table/OrderDetails.Table.sql
@@ -0,0 +1,19 @@
+
+CREATE TABLE `Order Details` (
+  `OrderID` INT NOT NULL,
+  `ProductID` INT NOT NULL,
+  `UnitPrice` DECIMAL(19, 4) NOT NULL DEFAULT 0,
+  `Quantity` SMALLINT NOT NULL DEFAULT 1,
+  `Discount` DOUBLE NOT NULL DEFAULT 0,
+  PRIMARY KEY (`OrderID`, `ProductID`)
+);
+
+ALTER TABLE `Order Details` 
+ADD CONSTRAINT `FK_Order_Details_Orders` 
+FOREIGN KEY (`OrderID`) 
+REFERENCES `Orders` (`OrderID`);
+
+ALTER TABLE `Order Details` 
+ADD CONSTRAINT `FK_Order_Details_Products` 
+FOREIGN KEY (`ProductID`) 
+REFERENCES `Products` (`ProductID`);
