68cd91e99b12497bbc62955c8bb1845b3c2efaa2
Purgo AI <support@purgo.ai>
2024-10-16 10:27:57 -0500

    Commit message

--- dbo-mig/table/Orders.Table.sql
+++ dbo-mig/table/Orders.Table.sql
@@ -1,21 +1,25 @@
 
 CREATE TABLE Orders (
-  OrderID INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY, 
-  CustomerID STRING, 
-  EmployeeID INTEGER, 
-  OrderDate TIMESTAMP, 
-  RequiredDate TIMESTAMP, 
-  ShippedDate TIMESTAMP, 
-  ShipVia INTEGER, 
-  Freight DECIMAL(19,4) DEFAULT 0, 
-  ShipName STRING, 
-  ShipAddress STRING, 
-  ShipCity STRING, 
-  ShipRegion STRING, 
-  ShipPostalCode STRING, 
-  ShipCountry STRING, 
-  PRIMARY KEY (OrderID),
-  FOREIGN KEY (CustomerID) REFERENCES Customers(CustomerID),
-  FOREIGN KEY (EmployeeID) REFERENCES Employees(EmployeeID),
-  FOREIGN KEY (ShipVia) REFERENCES Shippers(ShipperID)
-)
+  OrderID INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY,
+  CustomerID STRING(5),
+  EmployeeID INTEGER,
+  OrderDate TIMESTAMP,
+  RequiredDate TIMESTAMP,
+  ShippedDate TIMESTAMP,
+  ShipVia INTEGER,
+  Freight DECIMAL(19, 4) DEFAULT 0,
+  ShipName STRING(40),
+  ShipAddress STRING(60),
+  ShipCity STRING(15),
+  ShipRegion STRING(15),
+  ShipPostalCode STRING(10),
+  ShipCountry STRING(15)
+);
+
+ALTER TABLE Orders ADD CONSTRAINT PK_Orders PRIMARY KEY (OrderID);
+
+ALTER TABLE Orders ADD FOREIGN KEY (CustomerID) REFERENCES Customers(CustomerID);
+
+ALTER TABLE Orders ADD FOREIGN KEY (EmployeeID) REFERENCES Employees(EmployeeID);
+
+ALTER TABLE Orders ADD FOREIGN KEY (ShipVia) REFERENCES Shippers(ShipperID);
