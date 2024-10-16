ba7341e9bd552fa213bb0e3a21a0a3b132296678
Purgo AI <support@purgo.ai>
2024-10-16 10:48:13 -0500

    Commit message

--- dbo-mig/view/OrderDetailsExtended.View.sql
+++ dbo-mig/view/OrderDetailsExtended.View.sql
@@ -2,6 +2,6 @@
 CREATE OR REPLACE VIEW `Order Details Extended` AS
 SELECT `Order Details`.OrderID, `Order Details`.ProductID, Products.ProductName, 
     `Order Details`.UnitPrice, `Order Details`.Quantity, `Order Details`.Discount, 
-    (`Order Details`.UnitPrice * Quantity * (1 - Discount)) AS ExtendedPrice
+    (`Order Details`.UnitPrice * `Order Details`.Quantity * (1 - `Order Details`.Discount)) AS ExtendedPrice
 FROM Products 
-INNER JOIN `Order Details` ON Products.ProductID = `Order Details`.ProductID;
+INNER JOIN `Order Details` ON Products.ProductID = `Order Details`.ProductID
