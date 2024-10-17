8ad46968fb569152a8fb750fdc9cacaf21eac00b
Purgo AI <support@purgo.ai>
2024-10-17 11:25:47 -0500

    Commit message

---
+++ dbo2-mig/view/SummaryofSalesbyQuarter.View.sql
@@ -0,0 +1,6 @@
+
+CREATE OR REPLACE VIEW `Summary_of_Sales_by_Quarter` AS
+SELECT Orders.ShippedDate, Orders.OrderID, Order_Subtotals.Subtotal
+FROM Orders INNER JOIN Order_Subtotals ON Orders.OrderID = Order_Subtotals.OrderID
+WHERE Orders.ShippedDate IS NOT NULL
+
