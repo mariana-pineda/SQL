548946287650ad3839b9d20a426dd9d1276de3a2
mariana-pineda <139593142+mariana-pineda@users.noreply.github.com>
2024-10-17 09:47:01 -0500

    Delete dbo10-mig directory

--- dbo10-mig/view/SummaryofSalesbyYear.View.sql
+++
@@ -1,5 +0,0 @@
-
-CREATE OR REPLACE VIEW `Summary_of_Sales_by_Year` AS
-SELECT Orders.ShippedDate, Orders.OrderID, `Order Subtotals`.Subtotal
-FROM Orders INNER JOIN `Order Subtotals` ON Orders.OrderID = `Order Subtotals`.OrderID
-WHERE Orders.ShippedDate IS NOT NULL;
