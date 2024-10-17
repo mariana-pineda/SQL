aff0f36e702fbf8de0c0e21c1525f2fc0502891c
Purgo AI <support@purgo.ai>
2024-10-17 14:50:49 -0500

    Commit message

---
+++ dbo1-mig/view/OrderSubtotals.View.sql
@@ -0,0 +1,5 @@
+
+CREATE OR REPLACE VIEW `Order Subtotals` AS
+SELECT `Order Details`.OrderID, SUM(CAST(`Order Details`.UnitPrice * Quantity * (1 - Discount) AS DECIMAL(19, 4))) AS Subtotal
+FROM `Order Details`
+GROUP BY `Order Details`.OrderID
