fbb712cfcc6c0f00d9158b929310f5fdbb4971ef
Purgo AI <support@purgo.ai>
2024-10-17 11:24:43 -0500

    Commit message

---
+++ dbo2-mig/table/Orders.Table.sql
@@ -0,0 +1,29 @@
+
+CREATE TABLE Orders (
+  OrderID INTEGER NOT NULL,
+  CustomerID STRING NULL,
+  EmployeeID INTEGER NULL,
+  OrderDate TIMESTAMP NULL,
+  RequiredDate TIMESTAMP NULL,
+  ShippedDate TIMESTAMP NULL,
+  ShipVia INTEGER NULL,
+  Freight DECIMAL(19, 4) NULL DEFAULT 0,
+  ShipName STRING NULL,
+  ShipAddress STRING NULL,
+  ShipCity STRING NULL,
+  ShipRegion STRING NULL,
+  ShipPostalCode STRING NULL,
+  ShipCountry STRING NULL
+);
+
+ALTER TABLE Orders ADD CONSTRAINT PK_Orders PRIMARY KEY (OrderID);
+
+ALTER TABLE Orders ADD CONSTRAINT FK_Orders_Customers 
+FOREIGN KEY (CustomerID) REFERENCES Customers (CustomerID);
+
+ALTER TABLE Orders ADD CONSTRAINT FK_Orders_Employees 
+FOREIGN KEY (EmployeeID) REFERENCES Employees (EmployeeID);
+
+ALTER TABLE Orders ADD CONSTRAINT FK_Orders_Shippers 
+FOREIGN KEY (ShipVia) REFERENCES Shippers (ShipperID);
+
