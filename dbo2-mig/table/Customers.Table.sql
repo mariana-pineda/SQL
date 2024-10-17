910708405ed6c37dc57c257f551c5bf0df02abad
Purgo AI <support@purgo.ai>
2024-10-17 11:24:31 -0500

    Commit message

---
+++ dbo2-mig/table/Customers.Table.sql
@@ -0,0 +1,16 @@
+
+CREATE TABLE Customers (
+  CustomerID STRING NOT NULL, 
+  CompanyName STRING NOT NULL, 
+  ContactName STRING, 
+  ContactTitle STRING, 
+  Address STRING, 
+  City STRING, 
+  Region STRING, 
+  PostalCode STRING, 
+  Country STRING, 
+  Phone STRING, 
+  Fax STRING
+) USING DELTA;
+
+ALTER TABLE Customers ADD CONSTRAINT PK_Customers PRIMARY KEY (CustomerID);
