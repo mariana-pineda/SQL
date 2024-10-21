1f7cbddeb1b36318132fe006ebb04db0c69c333c
Purgo AI <support@purgo.ai>
2024-10-21 13:58:26 -0500

    Commit message

--- dbo-mig/table/Customers.Table.sql
+++ dbo-mig/table/Customers.Table.sql
@@ -1,17 +1,16 @@
 
 CREATE TABLE Customers (
-    CustomerID STRING NOT NULL,
-    CompanyName STRING NOT NULL,
-    ContactName STRING,
-    ContactTitle STRING,
-    Address STRING,
-    City STRING,
-    Region STRING,
-    PostalCode STRING,
-    Country STRING,
-    Phone STRING,
-    Fax STRING
-);
-
-ALTER TABLE Customers
-ADD CONSTRAINT PK_Customers PRIMARY KEY (CustomerID);
+  CustomerID STRING NOT NULL, 
+  CompanyName STRING NOT NULL, 
+  ContactName STRING, 
+  ContactTitle STRING, 
+  Address STRING, 
+  City STRING, 
+  Region STRING, 
+  PostalCode STRING, 
+  Country STRING, 
+  Phone STRING, 
+  Fax STRING
+)
+USING delta
+TBLPROPERTIES ('delta.primaryKey' = 'CustomerID')
