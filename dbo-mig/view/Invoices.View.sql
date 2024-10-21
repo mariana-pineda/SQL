9647f3abe8c5a3098915fc2624a31efef9b11123
Purgo AI <support@purgo.ai>
2024-10-21 13:59:12 -0500

    Commit message

--- dbo-mig/view/Invoices.View.sql
+++ dbo-mig/view/Invoices.View.sql
@@ -1,42 +1,42 @@
 
 CREATE VIEW Invoices AS
 SELECT 
-    Orders.ShipName,
-    Orders.ShipAddress,
-    Orders.ShipCity,
-    Orders.ShipRegion,
-    Orders.ShipPostalCode,
-    Orders.ShipCountry,
-    Orders.CustomerID,
-    Customers.CompanyName AS CustomerName,
-    Customers.Address,
-    Customers.City,
-    Customers.Region,
-    Customers.PostalCode,
-    Customers.Country,
-    CONCAT(FirstName, ' ', LastName) AS Salesperson,
-    Orders.OrderID,
-    Orders.OrderDate,
-    Orders.RequiredDate,
-    Orders.ShippedDate,
-    Shippers.CompanyName AS ShipperName,
-    OrderDetails.ProductID,
-    Products.ProductName,
-    OrderDetails.UnitPrice,
-    OrderDetails.Quantity,
-    OrderDetails.Discount,
-    (OrderDetails.UnitPrice * Quantity * (1 - Discount)) AS ExtendedPrice,
+    Orders.ShipName, 
+    Orders.ShipAddress, 
+    Orders.ShipCity, 
+    Orders.ShipRegion, 
+    Orders.ShipPostalCode, 
+    Orders.ShipCountry, 
+    Orders.CustomerID, 
+    Customers.CompanyName AS CustomerName, 
+    Customers.Address, 
+    Customers.City, 
+    Customers.Region, 
+    Customers.PostalCode, 
+    Customers.Country, 
+    CONCAT(Employees.FirstName, ' ', Employees.LastName) AS Salesperson, 
+    Orders.OrderID, 
+    Orders.OrderDate, 
+    Orders.RequiredDate, 
+    Orders.ShippedDate, 
+    Shippers.CompanyName AS ShipperName, 
+    OrderDetails.ProductID, 
+    Products.ProductName, 
+    OrderDetails.UnitPrice, 
+    OrderDetails.Quantity, 
+    OrderDetails.Discount, 
+    (OrderDetails.UnitPrice * OrderDetails.Quantity * (1 - OrderDetails.Discount)) AS ExtendedPrice, 
     Orders.Freight
 FROM 
     Shippers 
 INNER JOIN 
     (Products 
-    INNER JOIN 
+     INNER JOIN 
         (Employees 
-        INNER JOIN 
+         INNER JOIN 
             (Customers 
-            INNER JOIN Orders ON Customers.CustomerID = Orders.CustomerID) 
-        ON Employees.EmployeeID = Orders.EmployeeID
-        INNER JOIN OrderDetails ON Orders.OrderID = OrderDetails.OrderID) 
-    ON Products.ProductID = OrderDetails.ProductID) 
+             INNER JOIN Orders ON Customers.CustomerID = Orders.CustomerID) 
+         ON Employees.EmployeeID = Orders.EmployeeID 
+         INNER JOIN OrderDetails ON Orders.OrderID = OrderDetails.OrderID) 
+     ON Products.ProductID = OrderDetails.ProductID) 
 ON Shippers.ShipperID = Orders.ShipVia;
