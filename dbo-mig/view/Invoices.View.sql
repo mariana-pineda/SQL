e38d2d74397ee9a298cb844024c7c602fbd984c3
Purgo AI <support@purgo.ai>
2024-10-21 16:11:20 -0500

    Commit message

--- dbo-mig/view/Invoices.View.sql
+++ dbo-mig/view/Invoices.View.sql
@@ -1,42 +1,16 @@
 
 CREATE VIEW Invoices AS
-SELECT 
-    Orders.ShipName, 
-    Orders.ShipAddress, 
-    Orders.ShipCity, 
-    Orders.ShipRegion, 
-    Orders.ShipPostalCode, 
-    Orders.ShipCountry, 
-    Orders.CustomerID, 
-    Customers.CompanyName AS CustomerName, 
-    Customers.Address, 
-    Customers.City, 
-    Customers.Region, 
-    Customers.PostalCode, 
-    Customers.Country, 
-    CONCAT(Employees.FirstName, ' ', Employees.LastName) AS Salesperson, 
-    Orders.OrderID, 
-    Orders.OrderDate, 
-    Orders.RequiredDate, 
-    Orders.ShippedDate, 
-    Shippers.CompanyName AS ShipperName, 
-    OrderDetails.ProductID, 
-    Products.ProductName, 
-    OrderDetails.UnitPrice, 
-    OrderDetails.Quantity, 
-    OrderDetails.Discount, 
-    (OrderDetails.UnitPrice * OrderDetails.Quantity * (1 - OrderDetails.Discount)) AS ExtendedPrice, 
-    Orders.Freight
-FROM 
-    Shippers 
-INNER JOIN 
-    (Products 
-     INNER JOIN 
-        (Employees 
-         INNER JOIN 
-            (Customers 
-             INNER JOIN Orders ON Customers.CustomerID = Orders.CustomerID) 
-         ON Employees.EmployeeID = Orders.EmployeeID 
-         INNER JOIN OrderDetails ON Orders.OrderID = OrderDetails.OrderID) 
-     ON Products.ProductID = OrderDetails.ProductID) 
-ON Shippers.ShipperID = Orders.ShipVia;
+SELECT Orders.ShipName, Orders.ShipAddress, Orders.ShipCity, Orders.ShipRegion, Orders.ShipPostalCode, 
+	Orders.ShipCountry, Orders.CustomerID, Customers.CompanyName AS CustomerName, Customers.Address, Customers.City, 
+	Customers.Region, Customers.PostalCode, Customers.Country, 
+	(CONCAT(FirstName, ' ', LastName)) AS Salesperson, 
+	Orders.OrderID, Orders.OrderDate, Orders.RequiredDate, Orders.ShippedDate, Shippers.CompanyName AS ShipperName, 
+	OrderDetails.ProductID, Products.ProductName, OrderDetails.UnitPrice, OrderDetails.Quantity, 
+	OrderDetails.Discount, 
+	(CAST(OrderDetails.UnitPrice * OrderDetails.Quantity * (1 - OrderDetails.Discount) AS DECIMAL(19, 4))) AS ExtendedPrice, Orders.Freight
+FROM 	Shippers 
+INNER JOIN Orders ON Shippers.ShipperID = Orders.ShipVia
+INNER JOIN Customers ON Customers.CustomerID = Orders.CustomerID
+INNER JOIN Employees ON Employees.EmployeeID = Orders.EmployeeID
+INNER JOIN OrderDetails ON Orders.OrderID = OrderDetails.OrderID
+INNER JOIN Products ON Products.ProductID = OrderDetails.ProductID;
