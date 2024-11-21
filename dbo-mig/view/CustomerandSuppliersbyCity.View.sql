0418b1a36309a18cfaeca2afcd0931fbd3d03a25
Purgo AI <support@purgo.ai>
2024-11-21 15:21:52 -0500

    Commit message

--- dbo-mig/view/CustomerandSuppliersbyCity.View.sql
+++ dbo-mig/view/CustomerandSuppliersbyCity.View.sql
@@ -2,6 +2,6 @@
 CREATE OR REPLACE VIEW `Customer and Suppliers by City` AS
 SELECT City, CompanyName, ContactName, 'Customers' AS Relationship 
 FROM Customers
-UNION 
+UNION
 SELECT City, CompanyName, ContactName, 'Suppliers' AS Relationship
-FROM Suppliers;
+FROM Suppliers
