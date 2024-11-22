c5e300b45efa90209fbf397704ff0561cd310c63
Purgo AI <support@purgo.ai>
2024-11-22 09:54:14 -0500

    Commit message

--- dbo-mig/view/SalesbyCatery.View.sql
+++ dbo-mig/view/SalesbyCatery.View.sql
@@ -1,19 +1,23 @@
 
 CREATE OR REPLACE VIEW `Sales by Catery` AS
 SELECT 
-  Cateries.CateryID, 
-  Cateries.CateryName, 
-  Products.ProductName, 
-  SUM(`Order Details Extended`.ExtendedPrice) AS ProductSales
+    Cateries.CateryID, 
+    Cateries.CateryName, 
+    Products.ProductName, 
+    SUM(Order_Details_Extended.ExtendedPrice) AS ProductSales
 FROM 
-  Cateries 
-  INNER JOIN Products ON Cateries.CateryID = Products.CateryID
-  INNER JOIN `Order Details Extended` ON Products.ProductID = `Order Details Extended`.ProductID
-  INNER JOIN Orders ON Orders.OrderID = `Order Details Extended`.OrderID
+    Cateries 
+    INNER JOIN (
+        Products 
+        INNER JOIN (
+            Orders 
+            INNER JOIN Order_Details_Extended ON Orders.OrderID = Order_Details_Extended.OrderID
+        ) ON Products.ProductID = Order_Details_Extended.ProductID
+    ) ON Cateries.CateryID = Products.CateryID
 WHERE 
-  Orders.OrderDate BETWEEN '1997-01-01' AND '1997-12-31'
+    Orders.OrderDate BETWEEN '1997-01-01' AND '1997-12-31'
 GROUP BY 
-  Cateries.CateryID, 
-  Cateries.CateryName, 
-  Products.ProductName
+    Cateries.CateryID, 
+    Cateries.CateryName, 
+    Products.ProductName
 
