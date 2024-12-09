b8a2facfe4aea1ca02cb1eb916d27fe5cc8c4291
Purgo AI <support@purgo.ai>
2024-12-09 14:48:43 -0500

    Commit message

---
+++ stg-mig/table/Employees.Table.sql
@@ -0,0 +1,25 @@
+
+CREATE TABLE Employees (
+  EmployeeID BIGINT NOT NULL, 
+  LastName STRING NOT NULL, 
+  FirstName STRING NOT NULL, 
+  Title STRING, 
+  TitleOfCourtesy STRING, 
+  BirthDate TIMESTAMP, 
+  HireDate TIMESTAMP, 
+  Address STRING, 
+  City STRING, 
+  Region STRING, 
+  PostalCode STRING, 
+  Country STRING, 
+  HomePhone STRING, 
+  Extension STRING, 
+  Photo BINARY, 
+  Notes STRING, 
+  ReportsTo BIGINT, 
+  PhotoPath STRING
+) USING DELTA;
+
+ALTER TABLE Employees ADD CONSTRAINT PK_Employees PRIMARY KEY (EmployeeID);
+
+ALTER TABLE Employees ADD CONSTRAINT FK_Employees_Employees FOREIGN KEY (ReportsTo) REFERENCES Employees (EmployeeID);
