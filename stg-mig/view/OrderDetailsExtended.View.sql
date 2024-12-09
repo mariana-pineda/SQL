a17ef12c09c40f77aad56a354ddcaa128215b440
Purgo AI <support@purgo.ai>
2024-12-09 14:50:11 -0500

    Commit message

---
+++ stg-mig/view/OrderDetailsExtended.View.sql
@@ -0,0 +1,8 @@
+
+CREATE VIEW `Order Details Extended` AS
+SELECT `Order Details`.OrderID, `Order Details`.ProductID, Products.ProductName,
+    `Order Details`.UnitPrice, `Order Details`.Quantity, `Order Details`.Discount, 
+    (`Order Details`.UnitPrice * Quantity * (1 - Discount)) AS ExtendedPrice
+FROM Products
+INNER JOIN `Order Details` ON Products.ProductID = `Order Details`.ProductID;
+-- ORDER BY `Order Details`.OrderID
