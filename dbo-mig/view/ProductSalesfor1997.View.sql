c64ea64886737bc7e3165c75b3eb1c1d40cffe3b
Purgo AI <support@purgo.ai>
2024-12-09 09:35:58 -0500

    Commit message

---
+++ dbo-mig/view/ProductSalesfor1997.View.sql
@@ -0,0 +1,24 @@
+
+CREATE VIEW `Product Sales for 1997` AS
+SELECT 
+  Cateries.CateryName, 
+  Products.ProductName,
+  SUM(CAST(("Order Details".UnitPrice * Quantity * (1 - Discount) / 100) AS DECIMAL(18, 2)) * 100) AS ProductSales
+FROM 
+  Cateries 
+INNER JOIN 
+  Products 
+ON 
+  Cateries.CateryID = Products.CateryID
+INNER JOIN 
+  Orders 
+ON 
+  Orders.OrderID = "Order Details".OrderID
+INNER JOIN 
+  `Order Details` 
+ON 
+  Products.ProductID = "Order Details".ProductID
+WHERE 
+  Orders.ShippedDate BETWEEN '1997-01-01' AND '1997-12-31'
+GROUP BY 
+  Cateries.CateryName, Products.ProductName
