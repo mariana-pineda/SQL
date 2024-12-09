52b35921dc51a23a39af9d5c87b943535aad7f60
mariana-pineda <139593142+mariana-pineda@users.noreply.github.com>
2024-12-09 09:27:46 -0500

    Delete Pubs-Databricks directory

--- Pubs-Databricks/table/CustomerDemographics.Table.sql
+++
@@ -1,9 +0,0 @@
-
-CREATE TABLE CustomerDemographics (
-  CustomerTypeID STRING NOT NULL, 
-  CustomerDesc STRING NULL
-) 
-USING DELTA;
-
-ALTER TABLE CustomerDemographics
-ADD CONSTRAINT PK_CustomerDemographics PRIMARY KEY (CustomerTypeID);
