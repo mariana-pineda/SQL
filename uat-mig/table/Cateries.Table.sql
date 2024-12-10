ec35b009a3b98443b87cc255b4bac1381a8c835f
Purgo AI <support@purgo.ai>
2024-12-10 12:04:37 -0500

    Commit message

---
+++ uat-mig/table/Cateries.Table.sql
@@ -0,0 +1,11 @@
+
+CREATE TABLE Cateries (
+  CateryID INT NOT NULL GENERATED ALWAYS AS IDENTITY,
+  CateryName STRING NOT NULL,
+  Description STRING NULL,
+  Picture BINARY NULL
+)
+USING DELTA
+OPTIONS (
+  primaryKey = 'CateryID'
+)
