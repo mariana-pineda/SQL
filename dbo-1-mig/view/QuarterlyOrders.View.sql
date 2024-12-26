6f59e7d3bf91a6b83580180ac9ac9534db686800
mariana-pineda <139593142+mariana-pineda@users.noreply.github.com>
2024-12-26 10:37:48 -0500

    Delete dbo-1-mig directory

--- dbo-1-mig/view/QuarterlyOrders.View.sql
+++
@@ -1,5 +0,0 @@
-
-CREATE OR REPLACE VIEW `Quarterly Orders` AS
-SELECT DISTINCT Customers.CustomerID, Customers.CompanyName, Customers.City, Customers.Country
-FROM Customers RIGHT JOIN Orders ON Customers.CustomerID = Orders.CustomerID
-WHERE Orders.OrderDate BETWEEN '1997-01-01' AND '1997-12-31';
