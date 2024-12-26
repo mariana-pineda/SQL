cc8d751b96078df478af6d3715777a021ca71213
mariana-pineda <139593142+mariana-pineda@users.noreply.github.com>
2024-12-26 10:39:11 -0500

    Delete dbo-mig directory

--- dbo-mig/table/Territories.Table.sql
+++
@@ -1,9 +0,0 @@
-
-CREATE TABLE Territories (
-  TerritoryID STRING NOT NULL,
-  TerritoryDescription STRING NOT NULL,
-  RegionID INT NOT NULL,
-  PRIMARY KEY (TerritoryID)
-);
-
-ALTER TABLE Territories ADD FOREIGN KEY (RegionID) REFERENCES Region (RegionID);
