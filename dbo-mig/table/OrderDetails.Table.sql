9790364cd6740588a469bfb57afc762c255f7ad2
Purgo AI <support@purgo.ai>
2025-01-22 09:16:22 -0500

    Commit message

---
+++ dbo-mig/table/OrderDetails.Table.sql
@@ -0,0 +1,11 @@
+
+CREATE TABLE Order_Details (
+    OrderID INT NOT NULL, 
+    ProductID INT NOT NULL, 
+    UnitPrice DECIMAL(19,4) NOT NULL DEFAULT 0, 
+    Quantity SMALLINT NOT NULL DEFAULT 1, 
+    Discount FLOAT NOT NULL DEFAULT 0, 
+    PRIMARY KEY (OrderID, ProductID),
+    FOREIGN KEY (OrderID) REFERENCES Orders(OrderID),
+    FOREIGN KEY (ProductID) REFERENCES Products(ProductID)
+) USING DELTA;
