6b754187d82a54d8b058e4f65334df0590cf2eac
mariana-pineda <139593142+mariana-pineda@users.noreply.github.com>
2025-01-28 10:31:51 -0500

    Delete dbo-mig directory

--- dbo-mig/view/OrderDetailsExtended.View.sql
+++
@@ -1,7 +0,0 @@
-
-CREATE VIEW `Order Details Extended` AS
-SELECT `Order Details`.OrderID, `Order Details`.ProductID, Products.ProductName, 
-    `Order Details`.UnitPrice, `Order Details`.Quantity, `Order Details`.Discount, 
-    (CAST(`Order Details`.UnitPrice * Quantity * (1 - Discount) AS DOUBLE)) AS ExtendedPrice
-FROM Products INNER JOIN `Order Details` ON Products.ProductID = `Order Details`.ProductID
---ORDER BY `Order Details`.OrderID
