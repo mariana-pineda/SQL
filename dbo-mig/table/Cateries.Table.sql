8684ad9670b90acdaac84b657815492f547822ac
Purgo AI <support@purgo.ai>
2025-02-05 11:14:04 -0500

    Commit message

--- dbo-mig/table/Cateries.Table.sql
+++ dbo-mig/table/Cateries.Table.sql
@@ -1,9 +1,8 @@
 
-CREATE TABLE `Cateries` (
-  `CateryID` INT NOT NULL AUTO_INCREMENT,
-  `CateryName` STRING(15) NOT NULL,
-  `Description` STRING,
-  `Picture` BINARY,
-  PRIMARY KEY (`CateryID`)
+CREATE TABLE Cateries (
+  CateryID INT NOT NULL GENERATED ALWAYS AS IDENTITY, 
+  CateryName STRING NOT NULL, 
+  Description STRING NULL, 
+  Picture BINARY NULL, 
+  PRIMARY KEY (CateryID)
 )
-
