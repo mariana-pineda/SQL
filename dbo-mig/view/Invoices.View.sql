79b41f3733f42e4fd5f64c9aaac91ce86e1695b0
Purgo AI <support@purgo.ai>
2025-02-05 11:15:02 -0500

    Commit message

--- dbo-mig/view/Invoices.View.sql
+++ dbo-mig/view/Invoices.View.sql
@@ -14,24 +14,30 @@ SELECT
   Customers.Region, 
   Customers.PostalCode, 
   Customers.Country, 
-  CONCAT(FirstName, ' ', LastName) AS Salesperson, 
+  CONCAT(Employees.FirstName, ' ', Employees.LastName) AS Salesperson, 
   Orders.OrderID, 
   Orders.OrderDate, 
   Orders.RequiredDate, 
   Orders.ShippedDate, 
   Shippers.CompanyName AS ShipperName, 
-  OrderDetails.ProductID, 
+  `Order Details`.ProductID, 
   Products.ProductName, 
-  OrderDetails.UnitPrice, 
-  OrderDetails.Quantity, 
-  OrderDetails.Discount, 
-  (CAST(OrderDetails.UnitPrice * Quantity * (1 - Discount) AS DECIMAL(18,2))) AS ExtendedPrice, 
+  `Order Details`.UnitPrice, 
+  `Order Details`.Quantity, 
+  `Order Details`.Discount, 
+  CAST(`Order Details`.UnitPrice * `Order Details`.Quantity * (1 - `Order Details`.Discount) AS DECIMAL(18, 2)) AS ExtendedPrice, 
   Orders.Freight
 FROM 
   Shippers 
-  INNER JOIN Orders ON Shippers.ShipperID = Orders.ShipVia
-  INNER JOIN Customers ON Customers.CustomerID = Orders.CustomerID
-  INNER JOIN Employees ON Employees.EmployeeID = Orders.EmployeeID
-  INNER JOIN "Order Details" AS OrderDetails ON Orders.OrderID = OrderDetails.OrderID
-  INNER JOIN Products ON Products.ProductID = OrderDetails.ProductID
+  INNER JOIN (
+    Products 
+    INNER JOIN (
+      Employees 
+      INNER JOIN (
+        Customers 
+        INNER JOIN Orders ON Customers.CustomerID = Orders.CustomerID
+      ) ON Employees.EmployeeID = Orders.EmployeeID
+      INNER JOIN `Order Details` ON Orders.OrderID = `Order Details`.OrderID
+    ) ON Products.ProductID = `Order Details`.ProductID
+  ) ON Shippers.ShipperID = Orders.ShipVia
 
