b12fb6ab4420388929eda415d787c275f77e924e
Purgo AI <support@purgo.ai>
2025-02-05 11:14:19 -0500

    Commit message

--- dbo-mig/table/Employees.Table.sql
+++ dbo-mig/table/Employees.Table.sql
@@ -1,6 +1,6 @@
 
 CREATE TABLE Employees (
-  EmployeeID INT GENERATED ALWAYS AS IDENTITY,
+  EmployeeID INT NOT NULL GENERATED ALWAYS AS IDENTITY,
   LastName STRING NOT NULL,
   FirstName STRING NOT NULL,
   Title STRING,
@@ -17,10 +17,9 @@ CREATE TABLE Employees (
   Photo BINARY,
   Notes STRING,
   ReportsTo INT,
-  PhotoPath STRING,
-  PRIMARY KEY (EmployeeID)
+  PhotoPath STRING
 );
 
-ALTER TABLE Employees
-ADD CONSTRAINT FK_Employees_Employees FOREIGN KEY (ReportsTo) REFERENCES Employees(EmployeeID);
+ALTER TABLE Employees ADD CONSTRAINT PK_Employees PRIMARY KEY (EmployeeID);
 
+ALTER TABLE Employees ADD CONSTRAINT FK_Employees_Employees FOREIGN KEY (ReportsTo) REFERENCES Employees (EmployeeID);
