58cc55eaf5d2e002da239e1ae6d1c7ce6fd74025
Purgo AI <support@purgo.ai>
2025-02-05 11:15:14 -0500

    Commit message

--- dbo-mig/view/ProductsAboveAveragePrice.View.sql
+++ dbo-mig/view/ProductsAboveAveragePrice.View.sql
@@ -1,6 +1,6 @@
 
-CREATE VIEW `Products Above Average Price` AS
-SELECT Products.ProductName, Products.UnitPrice
+CREATE OR REPLACE VIEW `Products Above Average Price` AS
+SELECT ProductName, UnitPrice
 FROM Products
-WHERE Products.UnitPrice > (SELECT AVG(UnitPrice) FROM Products);
--- Note: The ORDER BY clause is not included in the view as views do not support ordering in Databricks.
+WHERE UnitPrice > (SELECT AVG(UnitPrice) FROM Products)
+
