b06a556ebcd88f53652f4733e768ce0dccb8c8e7
Purgo AI <support@purgo.ai>
2025-02-06 09:03:52 -0500

    Commit message

--- dbo-mig/view/ProductsAboveAveragePrice.View.sql
+++ dbo-mig/view/ProductsAboveAveragePrice.View.sql
@@ -1,6 +1,9 @@
 
-CREATE OR REPLACE VIEW `Products Above Average Price` AS
-SELECT ProductName, UnitPrice
+CREATE VIEW `Products Above Average Price` AS
+SELECT Products.ProductName, Products.UnitPrice
 FROM Products
-WHERE UnitPrice > (SELECT AVG(UnitPrice) FROM Products)
-
+WHERE Products.UnitPrice > (
+  SELECT AVG(UnitPrice) 
+  FROM Products
+)
+--ORDER BY Products.UnitPrice DESC
