0bae713d63b9fa332ea6e9ef1b7ebd7b9d49416f
Purgo AI <support@purgo.ai>
2025-02-06 09:03:20 -0500

    Commit message

--- dbo-mig/view/CurrentProductList.View.sql
+++ dbo-mig/view/CurrentProductList.View.sql
@@ -3,4 +3,4 @@ CREATE OR REPLACE VIEW `Current Product List` AS
 SELECT Product_List.ProductID, Product_List.ProductName
 FROM Products AS Product_List
 WHERE Product_List.Discontinued = 0
-
+-- Note: The ORDER BY clause is omitted in the view definition as Databricks SQL does not support ordering in views
