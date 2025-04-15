8927ff45afbd331681ffe2c5a774a637f1d815cd
Purgo AI <support@purgo.ai>
2025-04-15 10:32:36 -0500

    Commit message

--- test/QA1-90_CLONE_Standardize_data_for_delivery_dt_column_in_f_order_table_testdata.sql
+++ test/QA1-90_CLONE_Standardize_data_for_delivery_dt_column_in_f_order_table_testdata.sql
@@ -1,54 +1,127 @@
-CREATE OR REPLACE TEMP VIEW valid_delivery_dt AS (
-    -- Validate delivery_dt to be Decimal(38,0) and in format YYYYMMDD
-    WITH data_validation AS (
-        SELECT
-            order_nbr,
-            delivery_dt,
-            CASE
-                WHEN delivery_dt BETWEEN 19000101 AND 20991231 THEN 'Valid'
-                ELSE 'Invalid delivery_dt format'
-            END AS validation_status
-        FROM purgo_playground.purgo_playground.f_order
-    )
-    SELECT * FROM data_validation WHERE validation_status = 'Valid'
-);
+WITH order_test_data AS (
+  SELECT
+    -- Happy path test data
+    'ORD001' AS order_nbr, 
+    1 AS order_type, 
+    20240910 AS delivery_dt,  -- Valid date
+    100.0 AS order_qty, 
+    20240321 AS sched_dt, 
+    20240401 AS expected_shipped_dt, 
+    20240405 AS actual_shipped_dt, 
+    'LINE001' AS order_line_nbr, 
+    'LOC001' AS loc_tracker_id, 
+    '123 Shipping Lane' AS shipping_add, 
+    100.0 AS primary_qty, 
+    0.0 AS open_qty, 
+    100.0 AS shipped_qty, 
+    'Standard Order' AS order_desc, 
+    'N' AS flag_return, 
+    'N' AS flag_cancel, 
+    NULL AS cancel_dt, 
+    NULL AS cancel_qty, 
+    '2024-03-21T00:00:00.000+0000' AS crt_dt, 
+    '2024-03-21T00:00:00.000+0000' AS updt_dt
 
--- Generate diverse test data with 20-30 records covering various test scenarios
-INSERT INTO purgo_playground.purgo_playground.f_order (
-    order_nbr, order_type, delivery_dt, order_qty, sched_dt, expected_shipped_dt,
-    actual_shipped_dt, order_line_nbr, loc_tracker_id, shipping_add, primary_qty,
-    open_qty, shipped_qty, order_desc, flag_return, flag_cancel, cancel_dt,
-    cancel_qty, crt_dt, updt_dt
+  UNION ALL
+
+  -- Edge case: Maximum boundary for decimal yyyymmdd
+  SELECT
+    'ORD002', 
+    9999, 
+    99991231, -- Max valid date
+    0.0, 
+    NULL,
+    NULL, 
+    NULL, 
+    'LINE002', 
+    'LOC002', 
+    '456 Shipping Ave', 
+    500.0, 
+    500.0, 
+    0.0, 
+    'Express Delivery', 
+    'Y', 
+    'Y', 
+    20240301, 
+    50.0, 
+    '2024-01-01T00:00:00.000+0000', 
+    '2024-01-01T00:00:00.000+0000'
+
+  UNION ALL
+
+  -- Error case: Invalid delivery_dt out of range
+  SELECT
+    'ORD003', 
+    3, 
+    20241332, -- Invalid date (13th month)
+    -10.0, 
+    NULL, 
+    NULL, 
+    NULL, 
+    'LINE003', 
+    'LOC003', 
+    '789 Shipping Blvd', 
+    0.0, 
+    10.0, 
+    -10.0, 
+    'Backordered', 
+    'N', 
+    'Y', 
+    20240515, 
+    10.0, 
+    '2024-02-15T00:00:00.000+0000', 
+    '2024-02-15T00:00:00.000+0000'
+
+  UNION ALL
+
+  -- Null handling scenario
+  SELECT
+    'ORD004', 
+    NULL, 
+    NULL, -- Null delivery_dt
+    250.0, 
+    NULL, 
+    NULL, 
+    NULL, 
+    'LINE004', 
+    'LOC004', 
+    '234 Shipping Road', 
+    250.0, 
+    0.0, 
+    250.0,
+    'Regular Order', 
+    'N', 
+    'N', 
+    NULL, 
+    NULL, 
+    '2024-04-05T00:00:00.000+0000', 
+    '2024-04-05T00:00:00.000+0000'
+
+  UNION ALL
+
+  -- Special characters scenario
+  SELECT
+    'ORD005', 
+    5, 
+    20241020, 
+    75.0, 
+    NULL, 
+    NULL, 
+    NULL, 
+    'LINE005', 
+    'LOC005', 
+    '890 Special St #$%', -- Special characters in address
+    75.0, 
+    0.0, 
+    75.0, 
+    'Special Characters in Desc ñáéíóú', -- Multi-byte characters
+    'N', 
+    'N', 
+    NULL, 
+    NULL, 
+    '2024-05-05T00:00:00.000+0000', 
+    '2024-05-05T00:00:00.000+0000'
 )
-VALUES
-    -- Happy path: Valid decimal date in YYYYMMDD format
-    ('A001', 1001, 20240321, 100.0, 20240315, 20240320, 20240321, '1001', 'LT001', 'Address1', 100.0, 0.0, 0.0, 'Order Description', 'N', 'N', 0, 0.0, '2024-03-01T00:00:00.000+0000', '2024-03-02T00:00:00.000+0000'),
 
-    -- Edge case: First day of valid date range
-    ('A002', 1002, 19000101, 200.0, 20240315, 20240320, 20240321, '1002', 'LT002', 'Address2', 200.0, 0.0, 0.0, 'Order Description', 'N', 'N', 0, 0.0, '2024-03-01T00:00:00.000+0000', '2024-03-02T00:00:00.000+0000'),
+SELECT * FROM order_test_data
 
-    -- Edge case: Last day of valid date range
-    ('A003', 1003, 20991231, 300.0, 20240315, 20240320, 20240321, '1003', 'LT003', 'Address3', 300.0, 0.0, 0.0, 'Order Description', 'N', 'N', 0, 0.0, '2024-03-01T00:00:00.000+0000', '2024-03-02T00:00:00.000+0000'),
-
-    -- Error case: Delivery date out of range
-    ('A004', 1004, 20249300, 400.0, 20240315, 20240320, 20240321, '1004', 'LT004', 'Address4', 400.0, 0.0, 0.0, 'Order Description', 'N', 'N', 0, 0.0, '2024-03-01T00:00:00.000+0000', '2024-03-02T00:00:00.000+0000'),
-
-    -- Error case: Invalid decimal format
-    ('A005', 1005, 12345678901234567890123456789012345678, 500.0, 20240315, 20240320, 20240321, '1005', 'LT005', 'Address5', 500.0, 0.0, 0.0, 'Order Description', 'N', 'N', 0, 0.0, '2024-03-01T00:00:00.000+0000', '2024-03-02T00:00:00.000+0000'),
-
-    -- NULL handling scenario
-    ('A006', 1006, NULL, 600.0, 20240315, 20240320, 20240321, '1006', 'LT006', 'Address6', 600.0, 0.0, 0.0, 'Order Description', 'N', 'N', 0, 0.0, '2024-03-01T00:00:00.000+0000', '2024-03-02T00:00:00.000+0000'),
-
-    -- Special character handling
-    ('A007', 1007, 20240910, 700.0, 20240315, 20240320, 20240321, '1007##', 'LT007*&', 'Addr\ess7$', 700.0, 0.0, 0.0, 'Order Description!@#$', 'N', 'N', 0, 0.0, '2024-03-01T00:00:00.000+0000', '2024-03-02T00:00:00.000+0000'),
-
-    -- Multi-byte character handling
-    ('A008', 1008, 20240910, 800.0, 20240315, 20240320, 20240321, '1008', 'LT008', '漢字Address8', 800.0, 0.0, 0.0, 'Order Descr漢字iption', 'N', 'N', 0, 0.0, '2024-03-01T00:00:00.000+0000', '2024-03-02T00:00:00.000+0000');
-
--- Use the CTE to validate data
-WITH delivery_dt_check AS (
-    SELECT
-        delivery_dt
-    FROM valid_delivery_dt
-)
-SELECT delivery_dt FROM delivery_dt_check;
